a17b281b68eabc5688723ea67a5c501ecae1c6c9
Nazmican Calik <nazmican@pm.me>
2021-04-01 16:40:05 +0200

    dict fix

--- ops/plot_utils.py
+++ ops/plot_utils.py
@@ -2,22 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def plot_accuracy(history,model_details):
-    plt.plot(history.accuracy)
-    plt.plot(history.val_accuracy)
+    plt.plot(history['accuracy'])
+    plt.plot(history['val_accuracy'])
     plt.title('model accuracy')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train','validation'], loc='upper left')
-    plt.savefig(f'results/plots/{model_details['backbone']}_{model_details['transformer_arch']}_lr_{model_details['lr']:.5f}_bs_{model_details['batch_size']}_accuracy.png',bbox_inches='tight')
+    plt.savefig('results/plots/{}_{}_lr_{:.5f}_bs_{}_accuracy.png'.format(model_details['backbone'],model_details['transformer_arch'],model_details['lr'],model_details['batch_size']),bbox_inches='tight')
 
 def plot_loss(history,model_details):
-    plt.plot(history.loss)
-    plt.plot(history.val_loss)
+    plt.plot(history['loss'])
+    plt.plot(history['val_loss'])
     plt.title('model loss')
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train','validation'], loc='upper left')
-    plt.savefig(f'results/plots/{model_details['backbone']}_{model_details['transformer_arch']}_lr_{model_details['lr']:.5f}_bs_{model_details['batch_size']}_loss.png',bbox_inches='tight')
+    plt.savefig('results/plots/{}_{}_lr_{:.5f}_bs_{}_loss.png'.format(model_details['backbone'],model_details['transformer_arch'],model_details['lr'],model_details['batch_size']),bbox_inches='tight')
 
 def plot_statistics(history,model_details):
     plot_accuracy(history,model_details)
